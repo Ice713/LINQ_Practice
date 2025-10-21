d3e40dcfad07887602812487597c91b18d94f2e0
Ice713 <icramzilruiz@gmail.com>
2025-10-21 17:56:35 +0800

    with Where

--- LINQ_Practice/Form1.vb
+++ LINQ_Practice/Form1.vb
@@ -1,5 +1,10 @@
 ﻿Public Class Form1
     Private Sub buttonClick_Click(sender As Object, e As EventArgs) Handles buttonClick.Click
         Dim numbers As List(Of Integer) = New List(Of Integer) From {1, 2, 3, 4, 5, 6}
+
+        Dim evenNumbers = numbers.Where(Function(n) n Mod 2 = 0)
+
+        Dim 
     End Sub
 End Class
+
